58b57df245e54cb77339133aa9cbc2126ce2a27b
Paul Blair <ciriwe@phobot.net>
2024-04-22 19:49:16 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exdn.Mixfile do
   def project do
     [
       app: :exdn,
-      version: "2.2.0",
+      version: "2.2.1",
       description: """
       a two-way translator between Elixir data structures and strings of data
       following the edn specification.
